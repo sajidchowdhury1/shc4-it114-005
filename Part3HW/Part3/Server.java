3e5d03bffb7d52f7cb9eef1c3dfccc88e34573fe
sajidchowdhury1 <shc4@njit.edu>
2023-10-10 20:53:28 -0400

    This is the completed homework and the implementation was done on Server.java

--- Part3HW/Part3/Server.java
+++ Part3HW/Part3/Server.java
@@ -8,6 +8,12 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Server {
+    // shc4 10/9/23 it114-005
+    // variable to hold data for the number guesser
+    private int randomNum = -1;
+    // variable to set coinFace to heads or tails
+    private String coinFace = "";
+
     int port = 3001;
     // connected clients
     private List<ServerThread> clients = new ArrayList<ServerThread>();
@@ -80,6 +86,64 @@ public class Server {
                     break;
                 }
             }
+            return true;
+        }
+        // shc4 10/9/23 it114-005
+        // Got help from Professor Toguel during office hours
+        // Number Guesser
+        // The takes three else if to run, One command to start the game and random value
+        // The second else if exits the game and sets randomNum to a different value
+        // The third else if work when they use the word guess first and when randomNum is not set to -1
+        // There are if and else if to check the it any of the clients got it right or wrong
+        else if (message.trim().equalsIgnoreCase("start game")){
+            randomNum = (int) (Math.random()*10)+1;
+            broadcast("Number Guesser: (Select and number between 1-10) ", clientId);
+            return true;
+        }
+        else if (message.trim().equalsIgnoreCase("exit game")){
+            broadcast("Game stopped", clientId);
+            randomNum = -1;
+            return true;
+        }
+        else if (message.startsWith("guess") && randomNum > -1){
+            try{
+                int x = Integer.parseInt(message.split(" ")[1]);
+                if(x == randomNum){
+                    broadcast(String.format("The correct number was %s and the user who guessed it was %s", randomNum, clientId), clientId);
+                    randomNum = (int) (Math.random()*10)+1;
+                    broadcast("Number Guesser: \n(Select and number between 1-10) ", clientId);
+                }
+                else if(x < randomNum || x > randomNum){
+                    broadcast(String.format("Incorrect Guess: %s from User %s", x, clientId), clientId);
+                }
+            }catch(NumberFormatException e){
+                broadcast("Invalid input, type a number", clientId);
+            }catch(ArrayIndexOutOfBoundsException e){
+                broadcast("Invalid input, type a number", clientId);
+            }
+            return true;
+        } // end of number guesser here
+        
+        // shc4 10/9/23 it114-005
+        // Coin toss starts here
+        // Checks for the word flip
+        // Once it runs val chooses a number between 0 and 1
+        // Once thats choosen, then the other if conditions will determine heads or tails
+        // Then it broadcasts the results of who did coin flip
+        else if (message.trim().equalsIgnoreCase("flip")){
+            // val is only used in the iteration flip was commended
+            int val = (int)(Math.random()*2);
+            if (val == 0){
+                // heads
+                coinFace = "Heads";
+            }
+            else if (val == 1){
+                // tails
+                coinFace = "Tails";
+            }
+            // tells everyone what coin the user landed on
+            broadcast(String.format("Coin flip was done by %s and landed on %s", clientId, coinFace), clientId);
+
             return true;
         }
         return false;
