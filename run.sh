e882976a9de2acf9cd21432fd6891208698664ae
Sajid Chowdhury <sajidchowdhury@Sajids-MacBook-Air.local>
2023-11-15 22:24:03 -0500

    new run script and Java UI files

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
+port=3000
+if [ -n "$3" ];
+then
+    port="$3"
+fi
 if [ "$2" = "server" ];
 then
-	java $1.server.Server
+    echo "port $port"
+    java $1.server.Server $port
 elif [ "$2" = "client" ];
 then
-	java $1.client.Client
+    java $1.client.Client
     # In Milestone3 changes Client to ClientUI
+elif [ "$2" = "ui" ];
+then
+    java $1.client.ClientUI
 else
-	echo "Must specify client or server"
+    echo "Must specify client or server"
 fi
